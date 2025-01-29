e68b4874cefa383ba34e04b7f91c78ba7fcd10ec
Terry Wilcox <terry.wilcox@mac.com>
2025-01-29 12:25:34 -0700

    Set up project with rebar3
    Despite likely objections, rebar3 is the build tool of choice.

---
+++ src/erdi_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc erdi public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(erdi_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    erdi_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
