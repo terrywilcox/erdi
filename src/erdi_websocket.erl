6a8b0f9ac90ac48656bbbc6db50e8ddf1673c5cd
Terry Wilcox <terry.wilcox@mac.com>
2025-01-30 22:17:44 -0700

    Move configuration into sys.config
    Most configuration is in sys.config.
    
    The cert configuration calls a function, which poses
    a configuration problem for later.
    
    TODO: implement hello with heart beat.

--- src/erdi_websocket.erl
+++ src/erdi_websocket.erl
@@ -28,20 +28,13 @@ callback_mode() ->
     [state_functions, state_enter].
 
 init([]) ->
-    io:format("erdi_websocket init~n"),
-    % put all this stuff in config
-    TLSOpts = [{verify, verify_peer}, {cacerts, public_key:cacerts_get()}],
-    WebsocketOpts = [{verify, verify_none}, {cacerts, public_key:cacerts_get()}],
-    Domain = "discord.com",
-    Port = 443,
-    Path = "/api/gateway",
-    Protocols = [http],
-    {ok, get_url, _Data = #{domain => Domain, port => Port,
-                            tls => TLSOpts, path => Path,
-                           wss => WebsocketOpts, protocols => Protocols}}.
+    Data = maps:from_list(application:get_all_env(erdi)),
+    Data1 = Data#{gateway_tls => [{verify, verify_peer}, {cacerts, public_key:cacerts_get()}]},
+    Data2 = Data1#{websocket_tls => [{verify, verify_none}, {cacerts, public_key:cacerts_get()}]},
+    {ok, get_url, Data2}.
 
-get_url(enter, _OldStateName, #{tls := TLSOpts, domain := Domain,
-                               port := Port, path := Path} = Data) ->
+get_url(enter, _OldStateName, #{gateway_tls := TLSOpts, gateway_domain := Domain,
+                               port := Port, gateway_path := Path} = Data) ->
 
     {ok, Conn} = gun:open(Domain, Port, #{tls_opts => TLSOpts}),
     {ok, _Protocol} = gun:await_up(Conn),
@@ -51,16 +44,18 @@ get_url(info, {gun_response, _Pid, _Ref, nofin, _Code, _Headers}, Data) ->
     {keep_state, Data};
 get_url(info, {gun_data, _Pid, _Ref, nofin, HttpData}, Data) ->
     #{<<"url">> := <<"wss://", Url/binary>>} = json:decode(HttpData),
-    io:format(user, "Url = ~p~n", [Url]),
     {keep_state, Data#{url => binary_to_list(Url)}};
 get_url(info, {gun_data, Pid, _Ref, fin, _HttpData}, Data) ->
     gun:close(Pid),
     {next_state, not_connected, Data}.
 
-not_connected(enter, _OldStateName, #{url := Url, port := Port, wss := TLSOpts, protocols := Protocols} = Data) ->
+not_connected(enter, _OldStateName, #{url := Url, port := Port,
+                                      websocket_tls := TLSOpts,
+                                      websocket_protocols := Protocols,
+                                      websocket_path := Path} = Data) ->
     {ok, Conn} = gun:open(Url, Port, #{tls_opts => TLSOpts, protocols => Protocols}),
     {ok, _Protocol} = gun:await_up(Conn),
-    gun:ws_upgrade(Conn, "/?v=10"),
+    gun:ws_upgrade(Conn, Path),
     {keep_state, Data};
 not_connected(info, {gun_upgrade, Pid, Ref, [<<"websocket">>], _Headers}, Data) ->
     {next_state, wait_hello, Data#{conn => Pid, ref => Ref}};
