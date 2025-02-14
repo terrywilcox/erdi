5a4957f3cab2c3b130b3527c2a64e76c0207e3d9
Terry Wilcox <terry.wilcox@mac.com>
2025-02-14 08:27:22 -0700

    Test action rows
    Action rows work.
    
    The complexity of building an action row manually will
    necessitate more scafflding.
    
    TODO:	records are now required

--- src/erdi_message_replier.erl
+++ src/erdi_message_replier.erl
@@ -22,11 +22,29 @@ handle_event({?TYPE_MESSAGE_CREATE, Message, #{user_id := UserId} = _Options}, S
       ok;
     _ ->
       ChannelId1 = maps:get(?CHANNEL_ID, Data),
-      Bob = create_message(ChannelId1, <<"Bob!">>),
-      #{?CHANNEL_ID := ChannelId, <<"id">> := MessageId} = Bob,
-      timer:sleep(3000),
-      edit_message(ChannelId, MessageId, <<"Ted!">>)
+      Content =
+        #{<<"content">> => <<"this is the message text!">>,
+          <<"components">> =>
+            [#{<<"type">> => 1,
+               <<"components">> =>
+                 [#{<<"type">> => 2,
+                    <<"label">> => <<"I do nothing.">>,
+                    <<"style">> => 1,
+                    <<"custom_id">> => <<"do_nothing">>}]},
+             #{<<"type">> => 1,
+               <<"components">> =>
+                 [#{<<"type">> => 3,
+                    <<"custom_id">> => <<"useless">>,
+                    <<"options">> =>
+                      [#{<<"label">> => <<"Cow">>, <<"value">> => <<"cow">>},
+                       #{<<"label">> => <<"Dog">>, <<"value">> => <<"dog">>}]}]}]},
+
+      _Phil = json:encode(Content),
+      _Bob = create_message(ChannelId1, Content)
   end,
+  %#{?CHANNEL_ID := ChannelId, <<"id">> := MessageId} = Bob,
+  %      timer:sleep(3000),
+  %      edit_message(ChannelId, MessageId, <<"Ted!">>)
   {ok, State};
 handle_event(_, State) ->
   {ok, State}.
@@ -49,6 +67,6 @@ create_message(ChannelId, Content) ->
   {Method, Path, Message} = erdi_rest:create_message(ChannelId, Content),
   erdi_http:send_await({Method, Path, Message}).
 
-edit_message(ChannelId, MessageId, Content) ->
-  {Method, Path, Message} = erdi_rest:edit_message(ChannelId, MessageId, Content),
-  erdi_http:send({Method, Path, Message}).
+%edit_message(ChannelId, MessageId, Content) ->
+%  {Method, Path, Message} = erdi_rest:edit_message(ChannelId, MessageId, Content),
+%  erdi_http:send({Method, Path, Message}).
